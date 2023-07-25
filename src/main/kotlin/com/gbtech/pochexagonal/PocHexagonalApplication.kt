951f099841563a7737e34d03dcb8d1cdfcc2cbe2
Gilvano Piontkoski <g.piontkoski@gmail.com>
2023-07-25 14:09:43 -0300

    feat: Criar projeto

---
+++ src/main/kotlin/com/gbtech/pochexagonal/PocHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.gbtech.pochexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/com/gbtech/pochexagonal/PocHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gbtech.pochexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocHexagonalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
